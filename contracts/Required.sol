8cf09672afa3fb7e517e4a4bf3ac3b3351eec265
omudho <omudhomonica@gmail.com>
2024-05-30 12:55:30 +0300

    constructor test

---
+++ contracts/Required.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+contract ExampleContract {
+    function mustNotBeFive(uint256 x) public pure returns (uint256) {
+        require(x != 5, "five is not valid");
+        return x * 2;
+    }
+}
