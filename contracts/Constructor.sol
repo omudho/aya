3e93da2c1c30d48f5ab9522226ec00857116105e
omudho <omudhomonica@gmail.com>
2024-05-30 12:54:48 +0300

    constructor test

--- contracts/Constructor.sol
+++ contracts/Constructor.sol
@@ -7,8 +7,11 @@ contract ERC20 {
     mapping(address => uint256) public balances;
 
     function setSomeonesBalance(address owner, uint256 amount) public {
+       //require(msg.sender == banker, 'Not the banker');
         if (msg.sender == banker) {
             balances[owner] = amount;
+        }else{
+            revert("This is not the banker");
         }
         // do nothing
     }
